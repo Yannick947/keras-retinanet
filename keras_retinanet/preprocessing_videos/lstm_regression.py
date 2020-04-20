2556aec2440e171db7fec66a83e58933920f360b
Yannick <yannick.schrempp@web.de>
2020-04-20 18:40:42 +0200

    Add visualization for predictions over ground truth

--- keras_retinanet/preprocessing_videos/lstm_regression.py
+++ keras_retinanet/preprocessing_videos/lstm_regression.py
@@ -1,52 +1,32 @@
 from keras.layers import LSTM, Dense
 import pandas as pd
 import random
-from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import keras as keras
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 
 import data_generator_videos as dgv
-
 
 def main():
 
     timestep_num, feature_num = dgv.get_cleaned_lengths()
     lstm_model = create_lstm(timestep_num, feature_num)
     datagen_train, datagen_test = dgv.create_datagen()
-    history = train(lstm_model, datagen_train, datagen_test)
+    history, model = train(lstm_model, datagen_train, datagen_test)
     plot(history)
+    visualize_predictions(model, datagen_test)
+
 
 def train(model, datagen_train, datagen_test=None):
 
-    history = model.fit_generator(validation_steps=1,
-                                  generator=datagen_train,
-                                  validation_data=datagen_test,
-                                  steps_per_epoch=1,
-                                  epochs=150,
+    history = model.fit_generator(validation_steps=8,
+                                  generator=datagen_train.datagen(),
+                                  validation_data=datagen_test.datagen(),
+                                  steps_per_epoch=30,
+                                  epochs=30,
                                   verbose=1,
                                   shuffle=False)
-    return history
-
-
-# def scale(data, standardize=False):
-#     global scaler
-    #TODO: Everything
-
-    # if not standardize:
-    #     scaler = MinMaxScaler(feature_range=(0, 1))
-    # else:
-    #     scaler = StandardScaler()
-
-    # data = pd.DataFrame(scaler.fit_transform(df_scale))
-
-    # return df_scale.join(data_rest)
-
-
-def rescale(data):
-    global scaler
-    return pd.DataFrame(scaler.inverse_transform(data), columns=data.columns)
+    return history, model
 
 
 def create_lstm(timesteps, features):
@@ -54,19 +34,39 @@ def create_lstm(timesteps, features):
     # input_shape is of dimension (timesteps, features)
     model.add(
         LSTM(
-            units=100,
-            activation='relu',
-            # recurrent_activation='relu'
+            kernel_initializer=keras.initializers.Zeros(),
+            recurrent_initializer=keras.initializers.Zeros(),
+            bias_initializer=keras.initializers.Zeros(),
+            units=30,
+            activation='tanh',
+            recurrent_activation='sigmoid',
             #TODO: check value 0.1 if its too small or too high
             kernel_regularizer = keras.regularizers.l2(0.1),
-            input_shape=(
-                timesteps,
-                features),
+            input_shape=(timesteps, features),
             return_sequences=False))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    model.add(Dense(1, activation='relu'))
+    model.compile(loss='mean_squared_error', optimizer=keras.optimizers.adam(), )
     model.summary()
     return model
+
+
+def visualize_predictions(model, datagen_test):
+    datagen_test.reset_label_states()
+    predictions = model.predict_generator(generator=datagen_test.datagen(), steps=10)
+    predictions_inverse = datagen_test.scaler.scaler_labels.inverse_transform(predictions)
+    y_true = datagen_test.scaler.scaler_labels.inverse_transform(datagen_test.get_labels())
+    plt.scatter(predictions_inverse, y_true)
+    plt.title('Predictions over ground truth')
+    plt.xlabel('Predictions')
+    plt.ylabel('Ground truth')
+
+    plt.xticks(np.arange(min(np.append(predictions_inverse, y_true)),
+                         max(np.append(predictions_inverse , y_true))))
+
+    plt.yticks(np.arange(min(np.append(predictions_inverse, y_true)),
+                         max(np.append(predictions_inverse , y_true))))
+    plt.show()
 
 
 def plot(history):
@@ -76,7 +76,6 @@ def plot(history):
     plt.plot(history.history['val_loss'], label='test')
     plt.legend()
     plt.show()
-    return
 
 if __name__ == '__main__':
     main() 
