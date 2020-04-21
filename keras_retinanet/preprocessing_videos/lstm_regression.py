f3952ea0f3483b905635b2ac901aeb14b464c9aa
Yannick <yannick.schrempp@web.de>
2020-04-21 22:45:10 +0200

    Fix hparams metrics logging

--- keras_retinanet/preprocessing_videos/lstm_regression.py
+++ keras_retinanet/preprocessing_videos/lstm_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import os
+from time import gmtime, strftime
 
 import tensorflow as tf
 from tensorflow.keras.layers import LSTM, Dense
@@ -12,7 +13,7 @@ from tensorboard.plugins.hparams import api as hp
 import data_generator_videos as dgv
 
 
-LOGDIR = os.path.join('tensorboard')
+LOGDIR_TOP = os.path.join('tensorboard\\')
 
 
 def main():
@@ -20,44 +21,54 @@ def main():
     timestep_num, feature_num = dgv.get_filtered_lengths()
     datagen_train, datagen_test = dgv.create_datagen()
     
-    hp_domains = create_hyperparams_domains()
+    hp_domains, metrics = create_hyperparams_domains()
+    with tf.summary.create_file_writer(LOGDIR_TOP).as_default():
+         hp.hparams_config(
+            hparams=list(hp_domains.values()),
+            metrics=metrics)
 
     for num_units in hp_domains['num_units'].domain.values:
-        for dropout_rate in (hp_domains['dropout'].domain.min_value, hp_domains['dropout'].domain.max_value):
-            for optimizer in hp_domains['optimizer'].domain.values:
-                hparams = {
-                    'num_units': num_units,
-                    'dropout': dropout_rate,
-                    'optimizer': optimizer,
-                }
-                lstm_model = create_lstm(timestep_num, feature_num, hparams)
-                history, model = train(lstm_model, datagen_train, hparams, datagen_test)
+        for regularizer in (hp_domains['regularizer'].domain.min_value, hp_domains['regularizer'].domain.max_value):
+            for activation in hp_domains['activation'].domain.values:
+                logdir = os.path.join(LOGDIR_TOP + strftime("%Y_%b_%d_%H_%M_%S", gmtime()))
 
-    
-    plot(history)
-    visualize_predictions(model, datagen_test)
+                hparams = {
+                    'num_units'             : num_units,
+                    'regularizer'           : regularizer,
+                    'activation'            : activation,
+                }
+
+                lstm_model = create_lstm(timestep_num, feature_num, hparams)
+                history, model = train(lstm_model, datagen_train, logdir, hparams, datagen_test)
+
+    # plot(history)
+    # visualize_predictions(model, datagen_test)
 
 
 def create_hyperparams_domains(): 
     '''
     '''
-    HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([16, 32, 64]))
+    HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([10, 40, 60, 100]))
     HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1, 0.2))
     HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
+    HP_ACTIVATION_KERNEL =  hp.HParam('activation', hp.Discrete(['relu', 'sigmoid']))
+    HP_KERNEL_REGULARIZER = hp.HParam('num_units', hp.RealInterval(0.0, 0.8))
 
-    hp_domains = {'num_units':HP_NUM_UNITS, 'dropout':HP_DROPOUT, 'optimizer':HP_OPTIMIZER}
-    METRIC_VAL_LOSS = 'val_loss'
+    HP_TRAIN_LOSS = hp.Metric("loss", group="train", display_name="training loss")
+    HP_VAL_LOSS   = hp.Metric("val_loss", group="validation", display_name="validation loss")
+                                    
+    hp_domains = {'num_units'           : HP_NUM_UNITS,
+                  'dropout'             : HP_DROPOUT,
+                  'optimizer'           : HP_OPTIMIZER, 
+                  'regularizer'         : HP_KERNEL_REGULARIZER, 
+                  'activation'          : HP_ACTIVATION_KERNEL,
+                  }
 
-    #TODO: Check if this is necessary when using callbacks
-    with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
-        hp.hparams_config(
-            hparams=[HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER],
-            metrics=[hp.Metric(METRIC_VAL_LOSS, display_name='val_loss')],
-        )   
+    metrics = [HP_TRAIN_LOSS, HP_VAL_LOSS]
 
-    return hp_domains
+    return hp_domains, metrics
 
-def create_callbacks(logdir=LOGDIR, hparams=None): 
+def create_callbacks(logdir, hparams=None): 
     '''
     '''
     callbacks = list()
@@ -67,18 +78,19 @@ def create_callbacks(logdir=LOGDIR, hparams=None):
     return callbacks
 
 
-
-def train(model, datagen_train, hparams=None, datagen_test=None):
+def train(model, datagen_train, logdir, hparams=None, datagen_test=None):
     '''
     '''
-    history = model.fit_generator(validation_steps=8,
+    history = model.fit_generator(validation_steps=5,
                                 generator=datagen_train.datagen(),
                                 validation_data=datagen_test.datagen(),
                                 steps_per_epoch=30,
                                 epochs=10,
                                 verbose=1,
-                                shuffle=True, 
-                                callbacks=create_callbacks(LOGDIR, hparams))
+                                shuffle=True,
+                                callbacks=create_callbacks(logdir, hparams)
+                                )
+
     return history, model
 
 
@@ -90,28 +102,30 @@ def create_lstm(timesteps, features, hparams):
     # input_shape is of dimension (timesteps, features)
     model.add(
         LSTM(
-            kernel_initializer=tf.keras.initializers.Zeros(),
-            recurrent_initializer=tf.keras.initializers.Zeros(),
-            bias_initializer=tf.keras.initializers.Zeros(),
-            units=hparams['num_units'],
-            activation='tanh',
-            recurrent_activation='sigmoid',
-            #TODO: check value 0.1 if its too small or too high
-            # kernel_regularizer = keras.regularizers.l2(0.3),
-            input_shape=(timesteps, features),
-            return_sequences=False))
+            kernel_initializer          = tf.keras.initializers.Zeros(),
+            recurrent_initializer       = tf.keras.initializers.Zeros(),
+            bias_initializer            = tf.keras.initializers.Zeros(),
+            units                       = hparams['num_units'],
+            activation                  = hparams['activation'],
+            recurrent_activation        = 'sigmoid',
+            kernel_regularizer          = tf.keras.regularizers.l2(hparams['regularizer']),
+            input_shape                 = (timesteps, features),
+            return_sequences            = False))
 
     model.add(Dense(1, activation='tanh'))
-    model.compile(loss='mean_squared_error', optimizer=hparams['optimizer'])
+    model.compile(loss='mean_squared_error', optimizer='adam')
     model.summary()
     return model
 
 
 def visualize_predictions(model, datagen_test):
     datagen_test.reset_label_states()
+
     predictions = model.predict_generator(generator=datagen_test.datagen(), steps=10)
     predictions_inverse = datagen_test.scaler.scaler_labels.inverse_transform(predictions)
+
     y_true = datagen_test.scaler.scaler_labels.inverse_transform(datagen_test.get_labels())
+
     plt.scatter(predictions_inverse, y_true)
     plt.title('Predictions over ground truth')
     plt.xlabel('Predictions')
